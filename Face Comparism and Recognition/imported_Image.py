c96006ab5825eae6c2537ad15762dc22bac1fdb4
KombatBakpen <66734882+KombatBakpen@users.noreply.github.com>
2025-02-16 23:08:36 +0100

    Delete Face Comparism and Recognition directory

--- Face Comparism and Recognition/imported_Image.py
+++
@@ -1,7 +0,0 @@
-from write_to_new_image import ConvertToSingleImage 
-def dBaseImage():
-    """Function to extract the combined image from the database."""
-    converter = ConvertToSingleImage()  
-    imgDatabase = converter.loadDatabase() 
-    return imgDatabase
-
